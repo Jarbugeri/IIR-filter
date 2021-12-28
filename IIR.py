06a665bc162aa7b45f8cfeb8746dd27b4a1b4ff9
Jarbugeri <40212000+Jarbugeri@users.noreply.github.com>
2021-12-28 15:45:04 -0300

    Correção erros

--- IIR.py
+++ IIR.py
@@ -17,7 +17,7 @@ class IIR:
         self.y1 = 0.0
         self.y2 = 0.0
     
-    def reset(self,a0,a1,a2,b0,b1,b2):
+    def reset(self):
         """reset filter"""
         self.u0 = 0.0
         self.u1 = 0.0
@@ -26,26 +26,25 @@ class IIR:
         self.y1 = 0.0
         self.y2 = 0.0
     
-    def coff_update(self,a0,a1,a2,b0,b1,b2):
+    def coff_update(self,a0,a1,a2,b1,b2):
         """Update filter coefficients"""
         self.a0 = a0
         self.a1 = a1
         self.a2 = a2
-        self.b0 = b0
         self.b1 = b1
         self.b2 = b2
 
-    def run_filter(self,u):
+    def run_filter(self,u0):
         self.u2 = self.u1
         self.u1 = self.u0
-        self.u0 = u        
+        self.u0 = u0        
         self.y2 = self.y1
         self.y1 = self.y0
-        self.y0 = self.b0 * (self.a0 * self.u0 +
-                             self.a1 * self.u1 +
-                             self.a2 * self.u2 +
-                             self.b1 * self.y1 +
-                             self.b2 * self.y2)
+        self.y0 = (self.a0 * self.u0 +
+                   self.a1 * self.u1 +
+                   self.a2 * self.u2 +
+                   self.b1 * self.y1 +
+                   self.b2 * self.y2 )
         return self.y0
 
 filtro = IIR()
